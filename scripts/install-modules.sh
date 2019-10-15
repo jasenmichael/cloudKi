57516d5522b4a24204b57571c6c76c5c1f823d84
jasenmichael <jasen@ready-builder.com>
2019-10-15 18:54:10 -0500

    edit install modules script

--- scripts/install-modules.sh
+++ scripts/install-modules.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $DIR
 
+echo "starting dir $PWD"
 STARTING_DIR=$PWD
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "script dir $SCRIPT_DIR"
+
 cd $SCRIPT_DIR
+cd ../
+git submodule update --init --recursive
+
+cd scripts
 
 echo "installing static server"
 cd ../modules/static-server
 # echo $PWD
-npm install
+# npm install
 
 echo "installing pm2-ui"
 cd ../pm2-ui
 # echo $PWD
-npm install
+# npm install
 
 echo "installing cloud client"
 cd ../cloud-client
 # echo $PWD
-npm install
+# npm install
 
 # echo $PWD
 cd $STARTING_DIR
