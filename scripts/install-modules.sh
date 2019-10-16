d5481fe1be6dd576ea422184496de3132d513b72
jasenmichael <jasen@ready-builder.com>
2019-10-15 21:35:21 -0500

    edit install scripts

--- scripts/install-modules.sh
+++ scripts/install-modules.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-echo "starting dir $PWD"
+# echo "starting dir $PWD"
 STARTING_DIR=$PWD
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -15,17 +14,17 @@ cd scripts
 echo "installing static server"
 cd ../modules/static-server
 # echo $PWD
-# npm install
+npm install
 
 echo "installing pm2-ui"
 cd ../pm2-ui
 # echo $PWD
-# npm install
+npm install
 
 echo "installing cloud client"
 cd ../cloud-client
 # echo $PWD
-# npm install
+npm install
 
 # echo $PWD
 cd $STARTING_DIR
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,57 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "installing"
 sleep 1
 
 #  check dependencies
-checkdependencies() {
-    echo "Node Version - node -v"
-    node -v
-    echo "NPM Version - npm -v"
-    npm -v
-    echo "PM2 Version - pm2 -v"
-    pm2 -v
-    echo "Git Version - git --version"
+without_sudo="true"
+verify(){
+    # echo "yo $1"
+    # check=`echo "$1" | grep -E ^\-?[0-9]*\.?[0-9]+$`
+    check=`command -v $1`
+    if [ "$check" != '' ]; then
+        $2
+    else
+        echo "$1 not installed"
+        if [ "$1" = "pm2" ]; then
+            echo "installing $1"
+            if [ "$without_sudo" = "true" ]; then
+                echo "installing without sudo"
+                echo "npm install -g pm2"
+                $without_sudo=false
+                npm install -g pm2
+                verify pm2 "pm2 -v"
+            else
+                echo "installing with sudo"
+                echo "sudo npm install -g pm2"
+                sudo npm install -g pm2
+                verify pm2 "pm2 -v"
+            fi
+        fi
+        exit 1
+    fi
+}
+
+checkDependencies() {
+    echo "Checking dependencies..."
+    echo "Node Version"
+    verify node "node -v"
+    echo "NPM Version"
+    verify npm "npm -v"
+    echo "PM2 Version"
+    verify pm2 "pm2 -v"
+    echo "Git Version"
+    verify git
     git --version | tr -d 'git version '
+    echo "Dependencies met.."
 }
 
-#  get repository
-getrepo() {
-    echo "Gitting repo"
-    # get repo
+installModules(){
+    echo "Installing Modules"
+    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    $SCRIPT_DIR/install-modules.sh
+    echo "Dependencies met"
 }
 
-checkdependencies
-echo "Dependencies met"
-getrepo
+checkDependencies
+installModules
 echo "installation complete"
