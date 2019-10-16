2bc82302372d3b2bea0b954f4e43cc858a023b9a
jasenmichael <jasen@ready-builder.com>
2019-10-16 02:45:50 -0500

    edit start-module.sh

--- scripts/start-modules.sh
+++ scripts/start-modules.sh
@@ -2,7 +2,10 @@
 
 STARTING_DIR=$PWD
 # echo "$STARTING_DIR"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPT_DIR
 cd ../modules
+echo "Configuring and starting Modules"
 if [ ! -f ".configured" ]; then
     touch .configured
 fi
@@ -21,10 +24,11 @@ for D in *; do
         fi
     fi
 done
+wait
 pm2 startup > tmp
 echo "$(tail -n +3 tmp)"
 eval "$(tail -n +3 tmp)" >/dev/null 2>&1
 pm2 save
-rm tmp
 
+rm tmp
 cd $STARTING_DIR
